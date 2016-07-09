3a331c9bf5c3c256cdf10b38cac661259ae24ab4
Gabriel Konat <gabrielkonat@gmail.com>
2016-07-09 21:03:29 +0200

    Make director executable. Cleanup.

--- setup.py
+++ setup.py
@@ -15,19 +15,17 @@ _DIRECTOR_UNPACK_DIR = os.path.join(os.path.dirname(__file__), 'eclipsegen')
 
 def _download_director():
   if os.path.isdir(_DIRECTOR_DIR):
-    print('Deleting existing P2 director at {}'.format(_DIRECTOR_DIR))
     shutil.rmtree(_DIRECTOR_DIR, ignore_errors=True)
   with tempfile.TemporaryFile(prefix='p2-director', suffix='.zip') as tmpfile:
     with urllib.request.urlopen(_DIRECTOR_URL) as response:
-      print('Downloading P2 director from {} to {}'.format(_DIRECTOR_URL, tmpfile.name))
       shutil.copyfileobj(response, tmpfile)
     with ZipFile(tmpfile) as zipFile:
-      print('Unpacking P2 director from {} to {}'.format(tmpfile.name, _DIRECTOR_UNPACK_DIR))
       zipFile.extractall(path=_DIRECTOR_UNPACK_DIR)
-      # os.chmod(executable, 0o744)
+  # Make director executable
+  os.chmod(os.path.join(_DIRECTOR_DIR, 'director'), 0o744)
+  os.chmod(os.path.join(_DIRECTOR_DIR, 'director.bat'), 0o744)
   # Delete unused Python file in Ant plugin which should not be compiled
   runantFile = os.path.join(_DIRECTOR_DIR, 'plugins', 'org.apache.ant_1.9.4.v201504302020', 'bin', 'runant.py')
-  print('Deleting unused Python file {} from P2 director'.format(runantFile))
   os.remove(runantFile)
 
 
@@ -47,6 +45,10 @@ class PostInstallCommand(install):
     install.run(self)
 
 
+dependencies = [
+  'requests>=2.10.0'
+]
+
 setup(
   name='eclipsegen',
   version='0.2.0',
@@ -56,9 +58,9 @@ setup(
   author_email='gabrielkonat@gmail.com',
   license='Apache 2.0',
   packages=['eclipsegen'],
-  install_requires=['requests>=2.10.0'],
+  install_requires=dependencies,
   test_suite='nose.collector',
-  tests_require=['nose>=1.3.7', 'requests>=2.10.0'],
+  tests_require=['nose>=1.3.7'] + dependencies,
   include_package_data=True,
   zip_safe=False,
   cmdclass={
