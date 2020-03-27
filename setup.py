e7a6eb6f63356f19a6deafb0b087f8deedc363c2
Gabriel Konat <gabrielkonat@gmail.com>
2020-03-27 13:50:33 +0100

    Make director scripts executable after installing, as they does not always seem to be executable.

--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
 from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+import os
 
 dependencies = [
   'requests~=2.7'
 ]
 
+class PostDevelopCommand(develop):
+  def run(self):
+    make_director_executable()
+
+class PostInstallCommand(install):
+  def run(self):
+    make_director_executable()
+
+_DIRECTOR_DIR = os.path.join(os.path.dirname(__file__), 'eclipsegen', 'director')
+
+def make_director_executable():
+  print("Making director executable")
+  os.chmod(os.path.join(_DIRECTOR_DIR, 'director'), 0o744)
+  os.chmod(os.path.join(_DIRECTOR_DIR, 'director.bat'), 0o744)
+
 setup(
   name='eclipsegen',
-  version='0.4.1',
+  version='0.4.2',
   description='Generate Eclipse instances in Python',
   url='http://github.com/Gohla/eclipsegen',
   author='Gabriel Konat',
@@ -18,4 +36,8 @@ setup(
   tests_require=['nose>=1.3.7'] + dependencies,
   include_package_data=True,
   zip_safe=False,
+  cmdclass={
+    'install': PostInstallCommand,
+    'develop': PostDevelopCommand
+  }
 )
