572b19590d000d2f9f0ca07f96d8eb3a7f50ac2d
Gabriel Konat <gabrielkonat@gmail.com>
2020-10-06 09:38:17 +0200

    Linux 32-bit is not supported any more. Bump version to 0.4.5.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def make_director_executable():
 
 setup(
   name='eclipsegen',
-  version='0.4.4',
+  version='0.4.5',
   description='Generate Eclipse instances in Python',
   url='http://github.com/Gohla/eclipsegen',
   author='Gabriel Konat',
